eaf43c1637149c9d83c89c4dff5a07a9735cbb96
msjh80311 <msjh80311@gmail.com>
2016-10-20 05:28:32 +0800

    tasks assignment

--- src/main.java
+++ src/main.java
@@ -79,6 +79,117 @@ public class main {
 		
 		return Distance;
 	}
+
+
+	private static List<List> TaskAssign(List<List> Detail){
+		List<Integer> Workload = Detail.get(0);
+		List<Float> Gamma = Detail.get(1);
+		List<List> OtherData = Detail.get(2);
+		
+		List<List> Schedule = new ArrayList<>();
+		int [] total_processingT = {0, 0, 0, 0, 0, 0, 0};
+		
+		//..........這應該有比較好的做法吧.............
+		List<Integer> Monday = new ArrayList<>();
+		List<Integer> Tuesday = new ArrayList<>();
+		List<Integer> Wednesday = new ArrayList<>();
+		List<Integer> Thursday = new ArrayList<>();
+		List<Integer> Friday = new ArrayList<>();
+		List<Integer> Saturday = new ArrayList<>();
+		List<Integer> Sunday = new ArrayList<>();
+		//........................................
+		
+		for(int i = 1; i < OtherData.size()+1; i++){
+			List<Integer> task_details = OtherData.get(i-1);
+			//System.out.println(task_details);
+			int max_rewards = 0;
+			int assign_to_day = -1;
+			for(int j = 0; j < 7; j++){
+				if(task_details.get(j) > max_rewards){
+					max_rewards = task_details.get(j);
+					assign_to_day = j;
+				}
+			}
+			switch(assign_to_day){
+				case 0:
+					Monday.add(i);
+					break;
+				case 1:
+					Tuesday.add(i);
+					break;
+				case 2:
+					Wednesday.add(i);
+					break;
+				case 3:
+					Thursday.add(i);
+					break;
+				case 4:
+					Friday.add(i);
+					break;
+				case 5:
+					Saturday.add(i);
+					break;
+				case 6:
+					Sunday.add(i);
+					break;
+			}
+			total_processingT[assign_to_day] += task_details.get(7);
+		}
+		
+		Schedule.add(Monday);
+		Schedule.add(Tuesday);
+		Schedule.add(Wednesday);
+		Schedule.add(Thursday);
+		Schedule.add(Friday);
+		Schedule.add(Saturday);
+		Schedule.add(Sunday);
+		
+		for(int j = 0; j < 7; j++){
+			List<Integer> recent_tasks = Schedule.get(j);
+			
+			while(total_processingT[j] > (Workload.get(j))){		// 某天的分配超出負荷
+				int remove_task = -1, move_to_day = -1;
+				int min_loss = 1000;
+				int time_change = 0;
+				for(int k = 0; k < recent_tasks.size(); k++){		// 原排於j天的所有任務k
+					int index = recent_tasks.get(k);
+					
+					List<Integer> task_details = OtherData.get(index-1);
+					int recent_rewards = task_details.get(j);
+					int inner_min_loss = 1000;
+					int inner_move_to_day = -1;
+					for(int t = j+1; t < 7; t++){
+						int diff = recent_rewards - task_details.get(t);
+						if(diff < inner_min_loss){
+							inner_min_loss = diff;
+							inner_move_to_day = t;
+						}
+					}
+					
+					if(inner_min_loss < min_loss){
+						min_loss = inner_min_loss;
+						remove_task = k;		// 	原該日排入的第k個任務
+						move_to_day = inner_move_to_day;
+						time_change = task_details.get(7);
+					}
+				}
+				int index = recent_tasks.get(remove_task);
+				List<Integer> temp = Schedule.get(move_to_day);
+				temp.add(index);
+				Schedule.set(move_to_day, temp);
+				recent_tasks.remove(remove_task);
+				
+				total_processingT[j] -= time_change;
+				total_processingT[move_to_day] += time_change;
+				
+				System.out.println(index + " from " + j + " to " + move_to_day);
+			}
+		}
+		
+		
+		return Schedule;
+	}
+
 	
 	// other functions, ex: find the largest blah blah blah
 
@@ -91,6 +202,12 @@ public class main {
 		String distancepath = "C:\\Users\\admin\\Desktop\\161018_data\\txt\\161018_real_data_distance.txt";
 		int length = 19;
 		System.out.print(ReadDistanceFile(distancepath, length));
+
+		
+		// task assignment
+		List<List> Schedule = TaskAssign(ReadFile(detailpath));
+		System.out.println(Schedule);
+		
 
 		// do something, greedy algorithm
 	}
