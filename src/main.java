79f8cd26b648d032de97427790b8ac219c9e9505
sharonWU0505 <astrumstella048@gmail.com>
2016-10-25 21:55:54 +0800

    arrange!!!! finally finished

--- src/main.java
+++ src/main.java
@@ -26,7 +26,7 @@ public class main {
 				for(int i = 0; i < locnum; i++){
 					Distance[locindex][i] = Integer.parseInt(distanceData[i]);
 				}
-				System.out.println(Arrays.toString(Distance[locindex]));
+//				System.out.println(Arrays.toString(Distance[locindex]));
 				locindex += 1;
 			}
 			
@@ -40,70 +40,72 @@ public class main {
 		return Distance;
 	}
 
-//	// read file of detail data
-//	private static List<List> ReadFile(String filepath){
-//		List<List> Detail = new ArrayList<>();
-//		List<Integer> Workload = new ArrayList<>();
-//		List<List> OtherData = new ArrayList<>();
-//		List<Float> Gamma = new ArrayList<>();
-//		
-//		try {
-//			FileReader fr = new FileReader(filepath);
-//			BufferedReader br = new BufferedReader(fr);
-//			String line;
-//			while((line = br.readLine()) != null) {
-//				if(line.contains("workload")) {
-//					String[] workloadData = line.split(",");
-//					Workload.add(Integer.parseInt(workloadData[1]));
-//				}
-//				else if(line.contains("gamma")) {
-//					String[] gammaData = line.split(",");
-//					Gamma.add(Float.parseFloat(gammaData[1]));
-//					
-//				}
-//				else if(line.contains("index")) {
-//					;
-//				}
-//				else {
-//					List<Integer> DetailData = new ArrayList<>();  // rewards, penalty, etc
-//					String[] detailData = line.split(",");
-//					for(int i = 0; i < detailData.length; i++){
-//						if(i > 2) {
-//							DetailData.add(Integer.parseInt(detailData[i]));
-//						}
-//					}
-//					OtherData.add(DetailData);
-//				}
-//	        }
-//			Detail.add(Workload);
-//			Detail.add(Gamma);
-//			Detail.add(OtherData);
-//			br.close();
-//		}
-//	    catch(IOException e) {
-//	    	System.out.println(e);
-//	    }
-//		
-//		return Detail;
-//	}
-//
-
-	public static void main(String[] args) {
-//		// read detail file
-//		String detailpath = "C:\\Users\\admin\\workspace\\greedy\\src\\161018_real_data.txt";
-//		System.out.print(ReadFile(detailpath));
-//		
-//		// task assignment
-//		List<List> Schedule = TaskAssign(ReadFile(detailpath));
-//		System.out.println(Schedule);
+	// Read Detail Data File
+	private static List<List> ReadDetailFile(String filepath){
+		List<List> Detail = new ArrayList<>();
+		List<Integer> Workload = new ArrayList<>();
+		List<List> OtherData = new ArrayList<>();
+		List<Float> Gamma = new ArrayList<>();
 		
-//		for(int i = 0; i < Schedule.size(); i++){
-//			TaskSequence TaskSequence = new TaskSequence(Schedule.get(i));
-////			TaskSequence.setUsed();
-//
-//			String distancepath = "C:\\Users\\admin\\workspace\\greedy\\src\\161018_real_data_distance.txt";
-//			int length = 19;
-//			TaskSequence.Sequence(TaskSequence.ReadDistanceFile(distancepath, length));
-//		}
+		try {
+			FileReader fr = new FileReader(filepath);
+			BufferedReader br = new BufferedReader(fr);
+			String line;
+			while((line = br.readLine()) != null) {
+				if(line.contains("workload")) {
+					String[] workloadData = line.split(",");
+					Workload.add(Integer.parseInt(workloadData[1]));
+				}
+				else if(line.contains("gamma")) {
+					String[] gammaData = line.split(",");
+					Gamma.add(Float.parseFloat(gammaData[1]));
+					
+				}
+				else if(line.contains("taskId")) {
+					;
+				}
+				else {
+					List<Integer> DetailData = new ArrayList<>();  // rewards, penalty, etc
+					String[] detailData = line.split(",");
+					for(int i = 0; i < detailData.length; i++){
+						if(i > 2) {
+							DetailData.add(Integer.parseInt(detailData[i]));
+						}
+					}
+					OtherData.add(DetailData);
+				}
+	        }
+			Detail.add(Workload);
+			Detail.add(Gamma);
+			Detail.add(OtherData);
+			br.close();
+		}
+	    catch(IOException e) {
+	    	System.out.println(e);
+	    }
+		
+		return Detail;
+	}
+
+
+	public static void main(String[] args) {		
+		// read detail file
+		String detailpath = "C:\\Users\\admin\\workspace\\greedy\\src\\data_1025.txt";
+//		System.out.print(ReadDetailFile(detailpath));
+		
+		// task assignment
+		TaskAssign Schedule = new TaskAssign(ReadDetailFile(detailpath));
+		List<List> tasklist = Schedule.getSchedule();
+		System.out.println("Schedule:\n" + tasklist);
+		System.out.println("Unassigned tasks:\n" + Schedule.getUnassignedTasks());
+		
+		// read detail file
+		String distancepath = "C:\\Users\\admin\\workspace\\greedy\\src\\161018_real_data_distance.txt";
+
+		for(int i = 0; i < tasklist.size() - 1; i++){
+			TaskSequence TaskSequence = new TaskSequence(tasklist.get(i));
+//			TaskSequence.setUsed();
+			TaskSequence.Sequence(ReadDistanceFile(distancepath));
+		}
 	}
 } 
