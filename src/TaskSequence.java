855edef8363064ad0f57cf6ebe2c7912f04b21c1
sharonWU0505 <astrumstella048@gmail.com>
2016-10-25 19:55:57 +0800

    arrange

--- src/TaskSequence.java
+++ src/TaskSequence.java
@@ -8,41 +8,72 @@ import java.util.List;
 
 public class TaskSequence {
 	// attribute
-	private List<Integer> TaskList = new ArrayList<Integer>();  // task list for a day
-	private int[][] Permutation;
-	private int Length;		// number of tasks in a day
-	private int Index = 0;  // for permutation
-	private int SetNum = 1; // for permutation
-	private int[] Set;		// for permutation
-	private boolean[] Used;	// for permutation
+	private List<Integer> tasklist = new ArrayList<Integer>();  // task list for a day	
+	private int length;		// number of tasks in a day
+	private int index = 0;  // for permutation
+	private int[] set;		// for permutation
+	private boolean[] used;	// for permutation
+	private int setnum = 1; // for permutation
+	private int[][] permutation;
 
-	// constructor
-	public TaskSequence(List<Integer> TaskList) {
-		this.TaskList = TaskList;
-		Length = TaskList.size();
-		Set = new int[Length];
-		Used = new boolean[Length];
-		for(int i = 1; i < Length + 1; i++){
-			SetNum *= i;
+	// Constructor
+	public TaskSequence(List<Integer> tasklist) {
+		this.tasklist = tasklist;
+		length = tasklist.size();
+		set = new int[length];
+		used = new boolean[length];
+		for(int i = 0; i < length; i++) {
+			used[i] = false;
 		}
-		Permutation = new int[SetNum][];
-		for(int i = 0; i < SetNum; i++) {
-			Permutation[i] = new int[Length];
+		for(int i = 1; i < length + 1; i++){
+			setnum *= i;  // get the number of sets
+		}
+		permutation = new int[setnum][];
+		for(int i = 0; i < setnum; i++) {
+			permutation[i] = new int[length];
 		}
 	}
 	
-	// get Sequence
-	public int[] Sequence(int[][] Distance) {
-		Permutation(0, Length);
+	// Set Used to false
+//	public void setUsed() {
+//		for(int i = 0; i < length; i++) {
+//			used[i] = false;
+//		}
+//	}
+	
+	// Produce Permutation
+	public void Permutation(int start, int end) {
+		if(start == end) {
+			for(int i = 0; i < end; i++) {
+				permutation[index][i] = tasklist.get(set[i]);
+			}
+//			System.out.println(Arrays.toString(permutation[index]));
+			index += 1;
+		}
+		else {
+			for(int i = 0; i < end; i++) {
+				if(!used[i]) {
+					used[i] = true;
+					set[i] = start;
+					Permutation(start + 1, end);
+					used[i] = false;
+				}
+			}
+		}
+	}
+	
+	// Get Task Sequence
+	public int[] Sequence(int[][] distance) {
+		Permutation(0, length);
 
 		int min_distance = 0;
 		int min_index = 0;
-		for(int i = 0; i < SetNum; i++) {
+		for(int i = 0; i < setnum; i++) {
 			int temp_distance = 0;
-			for(int j = 0; j < Length - 1; j++){
-				int indexi = Permutation[i][j]-1;
-				int indexj = Permutation[i][j+1]-1;
-				temp_distance += Distance[indexi][indexj];
+			for(int j = 0; j < length - 1; j++){
+				int indexi = permutation[i][j]-1;
+				int indexj = permutation[i][j+1]-1;
+				temp_distance += distance[indexi][indexj];
 			}
 			if(i == 0){
 				min_distance = temp_distance;
@@ -52,16 +83,10 @@ public class TaskSequence {
 				min_index = i;
 			}
 		}
-		System.out.print("min_distance: " + min_distance + "\n");
-		System.out.print("sequence: " + Arrays.toString(Permutation[min_index]) + "\n");
-		return Permutation[min_index];
-	}
-	
-	// set used
-	public void setUsed() {
-		for(int i = 0; i < Length; i++) {
-			Used[i] = false;
-		}
+
+		System.out.print("Min distance: " + min_distance + "\n");
+		System.out.print("Task sequence: " + Arrays.toString(permutation[min_index]) + "\n");
+		return permutation[min_index];
 	}
 	
 	// read data for distance file
@@ -92,65 +117,45 @@ public class TaskSequence {
 		return Distance;
 	}
 
-	public void Permutation(int start, int end) {
-		if(start == end) {
-			for(int i = 0; i < end; i++) {
-				Permutation[Index][i] = TaskList.get(Set[i]);
-			}
-//			System.out.println(Arrays.toString(Permutation[index]));
-			Index += 1;
-		}
-		else {
-			for(int i = 0; i < end; i++) {
-				if(!Used[i]) {
-					Used[i] = true;
-					Set[i] = start;
-					Permutation(start + 1, end);
-					Used[i] = false;
-				}
-			}
+	public static void main(String[] args) {
+		List<List> test = new ArrayList<>();
+		List<Integer> test1 = new ArrayList<>();
+		List<Integer> test2 = new ArrayList<>();
+		List<Integer> test3 = new ArrayList<>();
+		List<Integer> test4 = new ArrayList<>();
+		List<Integer> test5 = new ArrayList<>();
+		test1.add(17);
+		test1.add(18);
+		test1.add(2);
+		test1.add(3);
+		test.add(test1);
+		test2.add(16);
+		test2.add(14);
+		test2.add(1);
+		test.add(test2);
+		test3.add(19);
+		test3.add(13);
+		test3.add(7);
+		test3.add(8);
+		test.add(test3);
+		test4.add(4);
+		test4.add(15);
+		test4.add(5);
+		test4.add(6);
+		test.add(test4);
+		test5.add(9);
+		test5.add(10);
+		test5.add(11);
+		test5.add(12);
+		test.add(test5);
+		
+		for(int i = 0; i < test.size(); i++){
+			TaskSequence TaskSequence = new TaskSequence(test.get(i));
+//			TaskSequence.setused();
+
+			String distancepath = "C:\\Users\\admin\\workspace\\greedy\\src\\161018_real_data_distance.txt";
+			int length = 19;
+			TaskSequence.Sequence(TaskSequence.ReadDistanceFile(distancepath, length));
 		}
 	}
-
-//	public static void main(String[] args) {
-//		List<List> test = new ArrayList<>();
-//		List<Integer> test1 = new ArrayList<>();
-//		List<Integer> test2 = new ArrayList<>();
-//		List<Integer> test3 = new ArrayList<>();
-//		List<Integer> test4 = new ArrayList<>();
-//		List<Integer> test5 = new ArrayList<>();
-//		test1.add(17);
-//		test1.add(18);
-//		test1.add(2);
-//		test1.add(3);
-//		test.add(test1);
-//		test2.add(16);
-//		test2.add(14);
-//		test2.add(1);
-//		test.add(test2);
-//		test3.add(19);
-//		test3.add(13);
-//		test3.add(7);
-//		test3.add(8);
-//		test.add(test3);
-//		test4.add(4);
-//		test4.add(15);
-//		test4.add(5);
-//		test4.add(6);
-//		test.add(test4);
-//		test5.add(9);
-//		test5.add(10);
-//		test5.add(11);
-//		test5.add(12);
-//		test.add(test5);
-//		
-//		for(int i = 0; i < test.size(); i++){
-//			TaskSequence TaskSequence = new TaskSequence(test.get(i));
-//			TaskSequence.setUsed();
-//
-//			String distancepath = "C:\\Users\\admin\\workspace\\greedy\\src\\161018_real_data_distance.txt";
-//			int length = 19;
-//			TaskSequence.Sequence(TaskSequence.ReadDistanceFile(distancepath, length));
-//		}
-//	}
 }
