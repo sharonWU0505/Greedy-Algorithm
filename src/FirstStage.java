16ecc06607ac6405abfe7d8b004011921359c908
sharonWU0505 <astrumstella048@gmail.com>
2016-11-18 01:18:25 +0800

    Finish moving FirstStage FindMinCost

--- src/FirstStage.java
+++ src/FirstStage.java
@@ -67,5 +67,46 @@ public class FirstStage{
 		System.out.println("\n" + "FirstStageAssignment:\n" + "Schedule: " + Schedule);
 		System.out.println("---------------------------------------------------------------------------------");
 	}
-	// End First Stage Assignment
+
+	// FindMinCost: find out the minimum opportunity cost of a day
+	private List<Map<Integer, Float>> FindMinCost(){
+		List<List<Integer>> oriSchedule = Schedule;
+		List<List<Integer>> newSchedule = new ArrayList<>();
+		List<Map<Integer, Float>> minOPcost = new ArrayList<>();	// the minimum opportunity cost of each day
+
+		for(int j = 0; j < Weekdays; j++){
+			List<Integer> daySchedule = oriSchedule.get(j);
+			float day_min_op = 5000;
+			for(int i = 0; i < daySchedule.size(); i++){
+				int taskid = daySchedule.get(i);
+				List<Float> task_details = OtherData.get(taskid - 1);
+				float current_rewards = task_details.get(j);
+				float task_min_op = 5000;
+				for(int k = 0; k < Weekdays; k++){
+					if(k == j)
+						continue;
+					float op = current_rewards - task_details.get(k);
+					// Find minimum opportunity cost (task_min_op) of a task
+					if(op < task_min_op){
+						task_min_op = op;
+					}
+				}
+				// Find minimum opportunity cost (day_min_op) of a day
+				if(task_min_op < day_min_op){
+					day_min_op = task_min_op;
+				}
+			}
+			Map<Integer, Float> daywithop = new HashMap<Integer, Float>();
+			daywithop.put(j, day_min_op);
+			minOPcost.add(daywithop);
+			daySchedule.add(0, j + 1);		// tag to identify the weekday
+			newSchedule.add(daySchedule);
+		}
+
+//		System.out.println("minOPcost: " + minOPcost);
+//		System.out.println("newSchedule: " + newSchedule);
+		Schedule = newSchedule;
+		return minOPcost;
+	}
+
 }
